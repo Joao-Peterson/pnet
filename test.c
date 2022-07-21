cca40253d2692a1a47225e57bfd53067179c2a8c
peterson <jooxpeters@gmail.com>
2022-07-21 15:19:08 -0300

    fixes

--- test.c
+++ test.c
@@ -681,6 +681,7 @@ int main(int argc, char **argv){
         pnet_fire(pnet, NULL);
         pnet_fire(pnet, NULL);
         pnet_fire(pnet, NULL);
+        pnet_fire(pnet, NULL);
     }
 
     test(
@@ -697,77 +698,6 @@ int main(int argc, char **argv){
     test_summary();
 
     // test for rush conditions with the timed transitions
-
-
-
-    // pnet_t *pnet = pnet_new(5, 4, 1, 2, 3,
-    //     places_name_pnet,
-    //     "p1", "p2", "p3", "p4", "p5",
-    //     places_init_pnet,
-    //     1, 0, 0, 0, 1,
-    //     transitions_pnet,
-    //     "t1", "t2", "t3", "t4",
-    //     arcs_pnet,
-    //     -1,  0,  0,  0,
-    //      1, -1,  0,  1,
-    //      0,  1, -1,  0,
-    //      0,  0,  1, -1,
-    //      0,  0, -1,  0,
-    //     inhibit_arcs_pnet,
-    //      0,  0,  0,  0,
-    //      0,  0,  0,  0,
-    //      0,  0,  0,  0,
-    //      0,  0,  0,  0,
-    //      1,  0,  0,  0,
-    //     transitions_delay_pnet,
-    //      2,  0,  3,  0,
-    //     input_name_pnet,
-    //     "start", "go",
-    //     input_map_pnet,
-    //      pnet_event_pos_edge,  0,  0,  0,
-    //      0,  0,  0,  pnet_event_pos_edge,
-    //     output_name_pnet,
-    //     "s1", "s2", "s3",
-    //     output_map_pnet,
-    //      0,  0,  0,
-    //      1,  0,  0, 
-    //      0,  1,  0, 
-    //      0,  0,  1, 
-    //      0,  0,  0,
-    //     conditions_name_pnet,
-    //     "power",
-    //     conditions_map_pnet,
-    //      1,  0,  0,  0,
-    //     end_options_pnet
-    // ); 
-
-    // if(pnet == NULL){
-    //     printf("[%s.%i] [DEBUG] [pnet_new() failed!] \n", __FILE__, __LINE__);
-    //     return 0;
-    // }
-
-    // pnet_sense(pnet);
-    // pnet_print(pnet);
-
-    // matrix_int_t *inputs = matrix_new(2, 1, 0, 0);
-    // matrix_int_t *conditions = matrix_new(1, 1, 1);
-
-    // pnet_fire(pnet, inputs, conditions);
-    // pnet_sense(pnet);
-    // pnet_print(pnet);
-    // matrix_delete(inputs);
-
-    // inputs = matrix_new(2, 1, 1, 0);
-
-    // pnet_fire(pnet, inputs, conditions);
-    // pnet_sense(pnet);
-    // pnet_print(pnet);
-    // matrix_delete(inputs);
-
-
-    // matrix_delete(conditions);
-    // pnet_delete(pnet);
-
     return 0;
 }
 
